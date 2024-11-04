129018084c7f6e2eb98cd9ae708d4ee3010ecf46
rpallin0705 <145771366+rpallin0705@users.noreply.github.com>
2024-11-04 20:44:53 +0100

    SetPhone :Activity :View SharedPreferences Intent SetPhone

--- app/src/main/java/com/example/actividadev1/MainActivity.kt
+++ app/src/main/java/com/example/actividadev1/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.actividadev1
 
+
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.actividadev1.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,20 +35,60 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    companion object {
+        const val TAG = "--- SALIDA ---"
+    }
+
     private fun start() {
 
-        mainBinding.webButton.setOnClickListener{
-            println("hola")
-            val url = getString(R.string.web_url)
-            val intent = Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse(url)
+        mainBinding.webButton.setOnClickListener { openWeb() }
+        mainBinding.alarmButton.setOnClickListener {
+
+            val calendar = Calendar.getInstance()
+            calendar.add(Calendar.MINUTE, 2)
+
+            val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+                putExtra(AlarmClock.EXTRA_MESSAGE, "Capítulo nuevo!1")
+                putExtra(AlarmClock.EXTRA_HOUR, calendar.get(Calendar.HOUR_OF_DAY))
+                putExtra(AlarmClock.EXTRA_MINUTES, calendar.get(Calendar.MINUTE))
+                putExtra(AlarmClock.EXTRA_SKIP_UI, false)
             }
 
-            // Verifica que se puedaa menejaar este intent
             if (intent.resolveActivity(packageManager) != null) {
                 startActivity(intent)
+            } else {
+                Toast.makeText(
+                    this,
+                    "Hubo un problema y la aplicación Reloj no está funcionando",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
-
+        mainBinding.phoneButton.setOnClickListener{
+            val intent = Intent(this@MainActivity, SetPhone::class.java)
+            intent.apply { addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT) }
+            startActivity(intent)
+        }
     }
+
+    private fun openWeb() {
+
+
+        val url = getString(R.string.web_url)
+        Toast.makeText(this, "Abriendo navegador" + url, Toast.LENGTH_LONG)
+            .show()
+
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
+        }
+
+        // Verifica que se puedaa menejaar este intent
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.d(TAG, "error")
+        }
+    }
+
+
 }
