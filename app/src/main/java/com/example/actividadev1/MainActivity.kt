ffe916e2f204e27c4787c9a9586699be27b27905
rpallin0705 <145771366+rpallin0705@users.noreply.github.com>
2024-11-06 18:46:38 +0100

    ADD: Funcionaliades -> Abrir web, poner alarma y realizar llamada a número establecido por el usuario

--- app/src/main/java/com/example/actividadev1/MainActivity.kt
+++ app/src/main/java/com/example/actividadev1/MainActivity.kt
@@ -5,12 +5,8 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.AlarmClock
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.actividadev1.databinding.ActivityMainBinding
 import java.util.Calendar
 
@@ -21,80 +17,69 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Inflar la vista
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
-        enableEdgeToEdge()
 
+        enableEdgeToEdge()
         start()
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 
     companion object {
         const val TAG = "--- SALIDA ---"
     }
 
+    /**
+     * Función que crea los Listener de todos los botones del aactivity
+     */
     private fun start() {
-
         mainBinding.webButton.setOnClickListener { openWeb() }
-        mainBinding.alarmButton.setOnClickListener {
-
-            val calendar = Calendar.getInstance()
-            calendar.add(Calendar.MINUTE, 2)
-
-            /*
-            * val explicitIntent = Intent("com.example.action.APP_ACTION")
-            * explicitIntent.setPackage(context.getPackageName())
-            * context.startActivity(explicitIntent)
-            * */
-
-            val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
-                putExtra(AlarmClock.EXTRA_MESSAGE, "Capítulo nuevo!1")
-                putExtra(AlarmClock.EXTRA_HOUR, calendar[Calendar.HOUR_OF_DAY])
-                putExtra(AlarmClock.EXTRA_MINUTES, calendar[Calendar.MINUTE])
-                putExtra(AlarmClock.EXTRA_SKIP_UI, false)
-            }
-
-            if (intent.resolveActivity(packageManager) != null) {
-                startActivity(intent)
-            } else {
-                Toast.makeText(
-                    this,
-                    "Hubo un problema y la aplicación Reloj no está funcionando",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }
-        mainBinding.phoneButton.setOnClickListener {
-            val intent = Intent(this@MainActivity, SetPhone::class.java)
-            intent.apply { addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT) }
-            startActivity(intent)
-        }
+        mainBinding.alarmButton.setOnClickListener { setAlarm() }
+        mainBinding.phoneButton.setOnClickListener { openPhoneSetter() }
     }
 
+    /**
+     * Abre un segundo activity para configurar el número de teléfono. Si el número de
+     * teléfono ha sido configurado previamente se realizará una comprobación en el segundo
+     * activity y este te mandará al activity que realiza la llamada
+     */
+    private fun openPhoneSetter() {
+        val intent = Intent(this@MainActivity, SetPhoneActivity::class.java)
+        intent.apply { addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT) }
+        startActivity(intent)
+    }
+
+
+    /**
+     * Crea una alarma dos minutos por delante de la hora actual del sistema,
+     * te manda a la aplicación reloj para confirmarla. Si establecemos laa banderaa
+     * EXTRA_SKIP_UI a false pondrá la alarma sin necesidad de abrir el Reloj
+     */
+    private fun setAlarm() {
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.MINUTE, 2)
+
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, "Capítulo nuevo!")
+            putExtra(AlarmClock.EXTRA_HOUR, calendar[Calendar.HOUR_OF_DAY])
+            putExtra(AlarmClock.EXTRA_MINUTES, calendar[Calendar.MINUTE])
+            // Si le pasamos true pondrá la alarma sin necesidad de abrir la aplicación del reloj
+            putExtra(AlarmClock.EXTRA_SKIP_UI, false)
+        }
+
+        startActivity(intent)
+    }
+
+
+    /**
+     * Abre un enlace web a la url que está establecidad en el fichero strings.xml
+     */
     private fun openWeb() {
-
-
         val url = getString(R.string.web_url)
-        Toast.makeText(this, "Abriendo navegador" + url, Toast.LENGTH_LONG)
-            .show()
-
         val intent = Intent(Intent.ACTION_VIEW).apply {
             data = Uri.parse(url)
         }
 
-        // Verifica que se puedaa menejaar este intent
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        } else {
-            Log.d(TAG, "error")
-        }
+        startActivity(intent)
     }
-
-
 }
